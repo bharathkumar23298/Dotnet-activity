9a969afe30320fbdcfe1bc3c7d9cd855ce2d69b9
Bharath Kumar <67777476+bharathkumar23298@users.noreply.github.com>
2021-10-29 11:21:32 +0530

    Add files via upload

---
+++ generic/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace Generics
+{
+    public class Myclass<T>
+    {
+        public bool campare(T a, T b)//(int a,int b)
+        {
+            if (a.Equals(b))//(a==b)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Myclass<int> check = new Myclass<int>();
+            bool res = check.campare('A', 'B');//(6,8)
+            Console.WriteLine(res);
+            //Console.Readkey();
+        }
+    }
+}
