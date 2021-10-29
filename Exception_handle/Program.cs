19930a436c35d02d93aadd6da71e9aaa9df01d59
Bharath Kumar <67777476+bharathkumar23298@users.noreply.github.com>
2021-10-29 11:59:54 +0530

    Add files via upload

---
+++ Exception_handle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace exception
+{
+    class Handling
+    {
+        static void Main(String[] args)
+        {
+            try 
+            {
+                Console.WriteLine("Enter the number:");
+                var num=int.Parse(Console.ReadLine());
+                Console.WriteLine($"square of{num} is{num *num}");
+            }
+            catch(Exception ex)
+            {
+               Console.Write("Error info:"+ex.Message);
+            }
+            finally
+            {
+               Console.Write("Re-try with a different number "); 
+            }
+        }
+    }
+}
