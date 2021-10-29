0cc0af252cc3ee79848435d0329b542d489e5789
Bharath Kumar <67777476+bharathkumar23298@users.noreply.github.com>
2021-10-29 11:42:07 +0530

    Add files via upload

---
+++ Type_conversion/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+ namespace TypeConversion
+{
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            int i=57;
+            float f=56.005f;
+            double d=567.76; // ACTIVITIES CHANGE FLOAT TO STRING
+            bool b=true;
+            
+           
+            Console.WriteLine(i.ToString());
+             Console.WriteLine(f.ToString());
+              Console.WriteLine(d.ToString());
+               Console.WriteLine(b.ToString());
+            Console.ReadKey();
+        }
+    }
+
+}
